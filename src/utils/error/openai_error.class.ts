65bd037352ee1bfe981d5b8d651efbda2d3c748d
Arcoz0308 <christopher@arcoz.dev>
2024-07-28 13:29:56 +0200

    Add OpenAIError class for detailed error handling in OpenAI API requests.

---
+++ src/utils/error/openai_error.class.ts
@@ -0,0 +1,52 @@
+import type { DebugValueString, ErrorOptions } from "arcscord";
+import { BaseError } from "arcscord";
+
+export type OpenAiErrorOptions = ErrorOptions & {
+  model: string;
+  send: string;
+  receive: string;
+  usedTokens: {
+    send: number;
+    receive: number;
+    total: number;
+  };
+}
+
+export class OpenAIError extends BaseError {
+
+  model: string;
+
+  send: string;
+
+  receive: string;
+
+  usedTokens: {
+    send: number;
+    receive: number;
+    total: number;
+  };
+
+  constructor(options: OpenAiErrorOptions) {
+    super(options);
+
+    this.model = options.model;
+    this.send = options.send;
+    this.receive = options.receive;
+    this.usedTokens = options.usedTokens;
+
+    this.name = "OpenAIError";
+  }
+
+  getDebugsString(): DebugValueString[] {
+    const debugs: DebugValueString[] = [];
+    debugs.push(...super.getDebugsString());
+
+    debugs.push(["Model", this.model]);
+    debugs.push(["Send", this.send]);
+    debugs.push(["Receive", this.receive]);
+    debugs.push(["Used Tokens", JSON.stringify(this.usedTokens)]);
+
+    return debugs;
+  }
+
+}

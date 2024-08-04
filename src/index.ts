771bdf6e3c35bb464b0daeb36d7aa638b606581c
Arcoz0308 <christopher@arcoz.dev>
2024-08-04 16:44:29 +0200

    Add search command handling in src/index.ts upon client ready event.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { env } from "./utils/env/env.util";
 import { AutoTreads } from "./events/auto_threads/auto_threads.class";
 import { NewLinkThreadName } from "./components/new_link_thread_name/new_link_thread_name.class";
 import { RenameLinkThread } from "./components/rename_link_thread/rename_link_thread.class";
+import { searchX } from "./utils/search/x/x.util";
+import { SearchCommand } from "./commands/search/search.class";
 
 const client = new ArcClient(env.TOKEN, {
   intents: [
@@ -19,5 +21,11 @@ client.componentManager.loadComponents([
   new NewLinkThreadName(client),
   new RenameLinkThread(client),
 ]);
+client.on("ready", async() => {
+  const commands = [new SearchCommand(client)];
+  const data = client.commandManager.loadCommands(commands);
+  const apisCommands = await client.commandManager.pushGuildCommands(env.SERVER_ID, data);
+  client.commandManager.resolveCommands(commands, apisCommands);
 
+});
 void client.login();

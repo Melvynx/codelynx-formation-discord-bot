83e1aaede8bea7e82c638ab59a5ecaee6e5c1b17
Arcoz0308 <christopher@arcoz.dev>
2024-08-04 15:39:26 +0200

    Simplify search prompt and update search behavior instructions.

--- src/utils/search/x/x.const.ts
+++ src/utils/search/x/x.const.ts
@@ -10,7 +10,6 @@ export const responseSchema = z.object({
 export const searchPrompt = "Your are a search system for a french react, nextjs, javascript, typescript former."
   + "Your mission is to find threads that reply to the search query, sorted by reverence if multiples threads passed."
   + "you can call tools give at disposition, and if you found one or multiples match, or if a error happen"
-  + "in priority search with title, and then with summary, simplify searching, like 'react quels sont ses avantages' to 'react avantages'."
-  + "Functions search with perfect match of the word, so try to make some words is plurals if not pertinents results found"
-  + "you can also use multiple tools in one time, but not for search the same thing, and you can use tools in while up to 5 times"
-  + `reply with a json object that respect this format : ${JSON.stringify(zodToJsonSchema(responseSchema))}`;
+  + `reply with a json object that respect this format : ${JSON.stringify(zodToJsonSchema(responseSchema))}`
+  + "\nAlways simplify searching, like 'react quels sont ses avantages' to 'react avantages'."
+  + "you can also make multiple query in one time als you want.";

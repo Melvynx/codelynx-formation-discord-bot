e2c80f5d62b96d86d2f7ac01711e5cc00fed3f1f
Arcoz0308 <christopher@arcoz.dev>
2024-08-11 12:14:54 +0200

    Add DATABASE_URL, GOOGLE_API_KEY, and OPENAI_API_KEY to env schema

--- src/utils/env/env.z.ts
+++ src/utils/env/env.z.ts
@@ -4,4 +4,7 @@ export const envSchema = z.object({
   TOKEN: z.string(),
   LINKS_CHANNEL_ID: z.string(),
   SERVER_ID: z.string(),
-})
+  DATABASE_URL: z.string(),
+  GOOGLE_API_KEY: z.string(),
+  OPENAI_API_KEY: z.string(),
+});

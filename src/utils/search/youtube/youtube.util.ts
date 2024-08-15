caf00267a224911c16b898e3ce440eaa6a5baf47
Arcoz0308 <christopher@arcoz.dev>
2024-08-15 10:27:55 +0200

    Return VideoInfo objects instead of just URLs in youtubeSearch function.

--- src/utils/search/youtube/youtube.util.ts
+++ src/utils/search/youtube/youtube.util.ts
@@ -3,24 +3,32 @@ import { env } from "../../env/env.util";
 import type { Result } from "arcscord";
 import { anyToError, BaseError, error } from "arcscord";
 import { ok } from "arcscord";
+import type { VideoInfo } from "./youtube.type";
 
 const client = youtube({
   version: "v3",
   auth: env.GOOGLE_API_KEY,
 });
 
-export const youtubeSearch = async(term: string): Promise<Result<string[], BaseError>> => {
+export const youtubeSearch = async(term: string): Promise<Result<VideoInfo[], BaseError>> => {
   try {
 
     const request = await client.search.list({
       channelId: "UC5HDIVwuqoIuKKw-WbQ4CvA",
       maxResults: 5,
-      part: ["snippet"],
+      part: ["id", "snippet"],
       q: term,
       type: ["video"],
     });
     const list = request.data.items || [];
-    return ok(list.map((l) => "https://www.youtube.com/watch?v=" + l.id?.videoId));
+    return ok(list.map((l) => {
+      const info:  VideoInfo =  {
+        url: "https://www.youtube.com/watch?v=" + l.id?.videoId,
+        id: l.id?.videoId || "no_id",
+        title: l.snippet?.title || "no_title",
+      };
+      return info;
+    }));
   } catch (e) {
     return error(new BaseError({
       message: "failed to request youtube error",

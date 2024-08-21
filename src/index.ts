48e0c4f4a7e21435000b3d6b75b979e63bcaf93f
Arcoz0308 <christopher@arcoz.dev>
2024-08-21 20:43:45 +0200

    Add AdminCommand, EmailInputModal, and VerifyButton to the client setup

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import { NewLinkThreadName } from "./components/new_link_thread_name/new_link_th
 import { RenameLinkThread } from "./components/rename_link_thread/rename_link_thread.class";
 import { SearchCommand } from "./commands/search/search.class";
 import { DetailedSearchResult } from "./components/detailled_search_result/detailed_search_result.class";
+import { AdminCommand } from "./commands/admin/admin.class";
+import { EmailInputModal } from "./components/email_input/email_input.class";
+import { VerifyButton } from "./components/verify_button/verify.button.class";
 
 const client = new ArcClient(env.TOKEN, {
   intents: [
@@ -21,10 +24,15 @@ client.componentManager.loadComponents([
   new NewLinkThreadName(client),
   new RenameLinkThread(client),
   new DetailedSearchResult(client),
+  new EmailInputModal(client),
+  new VerifyButton(client),
 ]);
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 client.on("ready", async() => {
-  const commands = [new SearchCommand(client)];
+  const commands = [
+    new SearchCommand(client),
+    new AdminCommand(client),
+  ];
   const data = client.commandManager.loadCommands(commands);
   const apisCommands = await client.commandManager.pushGuildCommands(env.SERVER_ID, data);
   client.commandManager.resolveCommands(commands, apisCommands);

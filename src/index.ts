426c2a6733b937e08863518c034536524d87dd3c
Dercraker <antoine.capitain@gmail.com>
2024-08-21 23:35:10 +0200

    WIP Solution Command

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { ArcClient } from "arcscord";
-import { env } from "./utils/env/env.util";
-import { AutoTreads } from "./events/auto_threads/auto_threads.class";
+import { SolutionCommand } from "./commands/help/solution.class";
+import { SearchCommand } from "./commands/search/search.class";
 import { NewLinkThreadName } from "./components/new_link_thread_name/new_link_thread_name.class";
 import { RenameLinkThread } from "./components/rename_link_thread/rename_link_thread.class";
-import { searchX } from "./utils/search/x/x.util";
-import { SearchCommand } from "./commands/search/search.class";
+import { AutoTreads } from "./events/auto_threads/auto_threads.class";
+import { env } from "./utils/env/env.util";
 
 const client = new ArcClient(env.TOKEN, {
   intents: [
@@ -21,11 +21,13 @@ client.componentManager.loadComponents([
   new NewLinkThreadName(client),
   new RenameLinkThread(client),
 ]);
-client.on("ready", async() => {
-  const commands = [new SearchCommand(client)];
+client.on("ready", async () => {
+  const commands = [new SearchCommand(client), new SolutionCommand(client)];
   const data = client.commandManager.loadCommands(commands);
-  const apisCommands = await client.commandManager.pushGuildCommands(env.SERVER_ID, data);
+  const apisCommands = await client.commandManager.pushGuildCommands(
+    env.SERVER_ID,
+    data,
+  );
   client.commandManager.resolveCommands(commands, apisCommands);
-
 });
 void client.login();
--- src/utils/env/env.z.ts
+++ src/utils/env/env.z.ts
@@ -1,7 +1,23 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const envSchema = z.object({
   TOKEN: z.string(),
-  LINKS_CHANNEL_ID: z.string(),
+
   SERVER_ID: z.string(),
-})
+  APPLICATION_ID: z.string(),
+
+  DATABASE_URL: z.string(),
+  OPENAI_API_KEY: z.string(),
+  GOOGLE_API_KEY: z.string(),
+  CODELINE_TOKEN: z.string(),
+  CODELINE_ENDPOINT: z.string(),
+
+  LINKS_CHANNEL_ID: z.string(),
+  HELP_CHANNEL_ID: z.string(),
+
+  MODERATOR_ROLE_ID: z.string(),
+  VERIFY_ROLE_ID: z.string(),
+  LYNX_ROLE_ID: z.string(),
+
+  RESOLVED_THREAD_TAG_ID: z.string(),
+});

3a62f09d50f20de1982fc63e7e035a8615fa1d3d
Arcoz0308 <christopher@arcoz.dev>
2024-08-21 20:42:24 +0200

    Add user type, schema, and utility functions for user API integration

---
+++ src/utils/api/codeline/codeline.dto.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const userSchema = z.object({
+  user: z.object({
+    discordId: z.string().optional().nullable(),
+    products: z.array(z.object({
+      id: z.string(),
+      title: z.string(),
+      discordRoleId: z.string(),
+    })),
+  }).optional().nullable(),
+});

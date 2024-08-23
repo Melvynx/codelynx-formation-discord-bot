c5e5d74da52ed55e3ff4830804511d7bc8b9699f
Dercraker <antoine.capitain@gmail.com>
2024-08-23 18:51:15 +0200

    add gif

--- src/events/solution/createThreadSolution.class.ts
+++ src/events/solution/createThreadSolution.class.ts
@@ -24,7 +24,10 @@ export class SolutionCreateThread extends Event<"threadCreate"> {
       .setDescription(
         ":white_check_mark: Une fois le post résolu, vous pouvez marquer le message qui vous a aidé avec : `clic droit -> Applications -> Marquer comme solution.`",
       )
-      .setColor("#5865f2");
+      .setColor("#5865f2")
+      .setImage(
+        "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXRzOXowMjF3c3IzNnJtc2RrcDB4a3J2YTdjNnp1MzNmMDhmY2Q5dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Gj1ZajxelyQtMQc4ar/giphy.gif",
+      );
 
     await thread.send({ embeds: [embed] });
 

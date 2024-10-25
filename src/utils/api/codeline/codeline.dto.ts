c58161baec07e64f3b1161d55605b764e1684cd3
Chris <christopher@arcoz.dev>
2024-10-25 23:53:10 +0200

    Refactor `userSchema` to include `id` and reformat `Member` model properties.

--- src/utils/api/codeline/codeline.dto.ts
+++ src/utils/api/codeline/codeline.dto.ts
@@ -1,16 +1,16 @@
 import { z } from "zod";
 
 export const userSchema = z.object({
-  user: z
-    .object({
-      discordId: z.string().optional().nullable(),
-      products: z.array(
-        z.object({
-          id: z.string(),
-          title: z.string(),
-        }),
-      ),
-    })
+  user: z.object({
+    id: z.string(),
+    discordId: z.string().optional().nullable(),
+    products: z.array(
+      z.object({
+        id: z.string(),
+        title: z.string(),
+      }),
+    ),
+  })
     .optional()
     .nullable(),
 });
